8f4b482cc52b6ad3f7346acb16c9003792ec711e
hyeon <hyeonjee0817@gmail.com>
2025-01-20 15:28:27 +0900

    [2025.01.20 수정사항]
    * Game Scene Power Button 추가

---
+++ Assets/Scripts/UI/GameBtnController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class GameBtnController : MonoBehaviour
+{
+    public void OnPowerBtnClick()
+    {
+        Debug.Log("Exiting the game");
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+    public void OnRestartBtnClick()
+    {
+        // TODO : Restart 함수 추가 필요 (2025.01.20)
+    }
+    public void OnFlipBtnClick()
+    {
+        // TODO : Flip 함수 추가 필요 (2025.01.20)
+    }
+}
